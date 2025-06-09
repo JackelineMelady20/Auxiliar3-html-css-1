3b1f27561cfdbadb022ad9b17531192906c09690
Jackeline Melady Peña <jackelinemeladypena07@gmail.com>
2025-06-09 00:02:59 -0500

    configurar

--- TODOproject/settings.py
+++ TODOproject/settings.py
@@ -21,6 +21,7 @@ mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
